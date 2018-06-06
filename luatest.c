42213b7cbaf7491c61ac9de729a15b905e1abd52
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-06-06 12:59:47 +0300

    Add coroutine test

--- luatest.c
+++ luatest.c
@@ -4,6 +4,7 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 struct HttpCtx {
   int i;
@@ -14,8 +15,11 @@ int http2_req(lua_State *lua){
         luaL_checktype(lua, 2, LUA_TTABLE);
         int last = lua_toboolean(lua, 3);
 
-        printf("Connecting to %s\n", addr);
-        printf("Last is %d\n", last);
+        if (last)
+        {
+            printf("Connecting to %s\n", addr);
+            printf("Last is %d\n", last);
+        }
 
         lua_pushvalue(lua, 2);
         lua_pushnil(lua);
@@ -24,7 +28,10 @@ int http2_req(lua_State *lua){
                 lua_pushvalue(lua, -2);
                 const char *key = lua_tostring(lua, -1);
                 const char *val = lua_tostring(lua, -2);
-                printf("%s => %s\n", key, val);
+                if (last)
+                {
+                        printf("%s => %s\n", key, val);
+                }
                 lua_pop(lua, 2);
         }
         lua_pop(lua, 1);
@@ -103,6 +110,50 @@ int luaopen_http2(lua_State *lua)
         return 1;
 }
 
+void luacoro(lua_State *lua)
+{
+  int it = 0;
+  luaL_dostring(lua, "co = coroutine.create(coro)");
+  do {
+    luaL_dostring(lua, "ok,args = coroutine.resume(co)");
+    luaL_dostring(lua, "return ok");
+    it++;
+  } while (lua_toboolean(lua, -1));
+  if (it != 5)
+  {
+    printf("it %d\n", it);
+    exit(1);
+  }
+}
+
+void luacoroperf(lua_State *lua)
+{
+  int i;
+  struct timeval tv1, tv2;
+  gettimeofday(&tv1, NULL);
+  for (i = 0; i < 10*1000; i++)
+  {
+    luacoro(lua);
+  }
+  gettimeofday(&tv2, NULL);
+  printf("Time %g us / iter\n",
+         (tv2.tv_sec-tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6)/10e3*1e6);
+}
+
+void luaperf(lua_State *lua)
+{
+  int i;
+  struct timeval tv1, tv2;
+  gettimeofday(&tv1, NULL);
+  for (i = 0; i < 100*1000; i++)
+  {
+    luaL_dostring(lua, "f()");
+  }
+  gettimeofday(&tv2, NULL);
+  printf("Time %g us / iter\n",
+         (tv2.tv_sec-tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6)/100e3*1e6);
+}
+
 int main(int argc, char **argv){
         lua_State *lua = luaL_newstate();
         luaL_openlibs(lua);
@@ -116,6 +167,20 @@ int main(int argc, char **argv){
         luaL_dostring(lua, "if true then");
         luaL_dostring(lua, "print(\"test successful\")");
         luaL_dostring(lua, "end");
+
+        printf("Coroutine test\n");
+
+        luacoro(lua);
+
+        printf("Perf test\n");
+
+        luaperf(lua);
+
+        printf("Coroutine perf test\n");
+
+        luacoroperf(lua);
+
+        printf("Entering command loop mode, press ^D to end\n");
 
         while ((nread = getline(&line, &len, stdin)) != -1) {
             if (strlen(line) != nread)
--- test.lua
+++ test.lua
@@ -17,3 +17,62 @@ while true do
 end
 
 Http2.free(h)
+
+function f()
+   local t
+   local h
+   local t2
+   t = {}
+   t["foo"] = "bar"
+   h = Http2.req("www.google.fi", t, false)
+   t2 = Http2.headers(t)
+   if t2["foo"] ~= "bar" then
+     print("Error")
+   end
+   if t2["bar"] ~= "foo" then
+     print("Error")
+   end
+   
+   t = Http2.datablk(h)
+   while true do
+     if t.last then
+       break
+     end
+     t = Http2.datablk(h)
+   end
+   Http2.free(h)
+end
+
+function coro2()
+   local t
+   local h
+   local t2
+   t = {}
+   t["foo"] = "bar"
+   h = Http2.req("www.google.fi", t, false)
+   coroutine.yield(1)
+   t2 = Http2.headers(t)
+   if t2["foo"] ~= "bar" then
+     print("Error")
+   end
+   if t2["bar"] ~= "foo" then
+     print("Error")
+   end
+
+   coroutine.yield(2)
+   
+   t = Http2.datablk(h)
+   while true do
+     if t.last then
+       break
+     end
+     coroutine.yield(3)
+     t = Http2.datablk(h)
+   end
+   Http2.free(h)
+   return -1
+end
+
+function coro()
+   return coro2()
+end
